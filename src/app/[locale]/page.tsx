7bfad34621f0b3fc603e291d8b3bfdf0f095bf14
az0319h <az0319h@naver.com>
2025-11-04 17:30:12 +0900

    feat: 로고 마퀴 애니메이션 구현 및 마퀴 에셋 추가

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,11 @@
+import LifestyleHero from "@/components/pages/home/LifestyleHero";
 import ValueHeroSection from "@/components/pages/home/ValueHeroSection";
 
 export default async function Home() {
   return (
     <>
       <ValueHeroSection />
+      <LifestyleHero />
     </>
   );
 }
