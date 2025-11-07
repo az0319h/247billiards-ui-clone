7eee7a6895b0e19e2db2fda40094791dbfbe3683
az0319h <az0319h@naver.com>
2025-11-07 14:29:25 +0900

    feat(home): AboutUs 섹션 추가 및 페이지 연결

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,6 @@
 import SectionLine from "@/components/common/SectionLine";
+import SectionLayout from "@/components/layout/SectionLayout";
+import AboutUsSection from "@/components/pages/home/AboutUsSection";
 import CollectionPreviewSection from "@/components/pages/home/CollectionPreviewSection";
 import DesignPitchSection from "@/components/pages/home/DesignPitchSection";
 import LifestyleHero from "@/components/pages/home/LifestyleHero";
@@ -15,7 +17,13 @@ export default async function Home() {
       <SectionLine />
       <CollectionPreviewSection />
       <SectionLine height={0} />
-      About
+      <SectionLayout>
+        <h2 className="text-[5rem] lg:text-[12rem] sm:text-[7.5rem] sm:leading-[7.5rem] lg:leading-[12rem] leading-[5rem]">
+          About—us
+        </h2>
+      </SectionLayout>
+      <SectionLine />
+      <AboutUsSection />
     </>
   );
 }
