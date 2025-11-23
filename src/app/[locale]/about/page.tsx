597b826e82fd5b066832a0a244d2f151f11114ba
az0319h <az0319h@naver.com>
2025-11-23 23:53:11 +0900

    feat(about): SectionOverflow 컴포넌트 추가 및 About 페이지 섹션 구성

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,20 @@
+import SectionLine from "@/components/common/SectionLine";
+import SectionOverflow from "@/components/common/SectionOverflow";
 import AboutHeroSection from "@/components/pages/about-us/AboutHeroSection";
 import ScrollHorizontalTitle from "@/components/pages/about-us/ScrollHorizontalTitle";
+import AboutUsSection from "@/components/pages/home/AboutUsSection";
 
-export default function Page() {
+export default function AboutPage() {
   return (
     <>
       <AboutHeroSection />
       <ScrollHorizontalTitle />
+      <SectionOverflow />
+      <SectionOverflow />
+      <SectionOverflow />
+      <AboutUsSection />
+      <SectionLine />
+      여기서 부터 작성
     </>
   );
 }
